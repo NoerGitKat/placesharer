7ba8adf61627d1c26e2a67301b50f9d8604692a2
Noer Paanakker <noer@lightworksweb.nl>
2020-02-28 15:50:40 +0100

    finalized update and delete places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,11 +1,23 @@
 const uuid = require('uuid/v4');
 const HttpError = require('./../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
 	{
 		id: 'p1',
 		title: 'Empire State',
 		description: 'Famous shit right here!',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+		address: '20 W 34th St, New York, NY 10001',
+		location: {
+			lat: 40.7484405,
+			lng: -73.9878584,
+		},
+		creator: 'u1',
+	},
+	{
+		id: 'panotherplace',
+		title: 'Empire State',
+		description: 'The same famous shit right here!',
 		imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
 		address: '20 W 34th St, New York, NY 10001',
 		location: {
@@ -27,15 +39,17 @@ const getPlaceById = (req, res) => {
 	}
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
 	const { userId } = req.params;
 
-	const place = DUMMY_PLACES.find(place => place.creator === userId);
+	const places = DUMMY_PLACES.filter(place => place.creator === userId);
 
-	if (place) {
-		res.status(200).json(place);
+	console.log('places', places);
+
+	if (places.length > 0) {
+		res.status(200).json(places);
 	} else {
-		const error = new HttpError('Could not find a place with the provided user ID!', 404);
+		const error = new HttpError('Could not find places with the provided user ID!', 404);
 		return next(error);
 	}
 };
@@ -48,9 +62,40 @@ const createPlace = (req, res, next) => {
 	// Push to fake DB
 	DUMMY_PLACES.push(createdPlace);
 
-	res.status(201).json(createdPlace);
+	res.status(201).json(DUMMY_PLACES);
+};
+
+const updatePlace = (req, res, next) => {
+	// Only allow title and description to be updated
+	const { title, description } = req.body;
+
+	const { placeId } = req.params;
+
+	// 1. Search through array for match
+	const updatedPlace = DUMMY_PLACES.find(place => place.id === placeId);
+
+	const placeIndex = DUMMY_PLACES.findIndex(place => place.id === placeId);
+
+	// 2. Update place in copy
+	updatedPlace.title = title;
+	updatedPlace.description = description;
+
+	// 3. Replace old with updated copy
+	DUMMY_PLACES[placeIndex] = updatedPlace;
+
+	res.status(200).json(DUMMY_PLACES);
+};
+
+const deletePlace = (req, res, next) => {
+	const { placeId } = req.params;
+
+	DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId);
+
+	res.status(200).json(DUMMY_PLACES);
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- backend/routes/placeRouter.js
+++ backend/routes/placeRouter.js
@@ -4,16 +4,22 @@ const placesRouter = express.Router();
 
 // Controllers
 const { getPlaceById } = require('./../controllers/places-controllers');
-const { getPlaceByUserId } = require('./../controllers/places-controllers');
+const { getPlacesByUserId } = require('./../controllers/places-controllers');
 const { createPlace } = require('./../controllers/places-controllers');
+const { updatePlace } = require('./../controllers/places-controllers');
+const { deletePlace } = require('./../controllers/places-controllers');
 
 placesRouter
 	.route('/')
 	.get((req, res) => res.json({ msg: 'it works!' }))
 	.post(createPlace);
 
-placesRouter.route('/:placeId').get(getPlaceById);
+placesRouter
+	.route('/:placeId')
+	.get(getPlaceById)
+	.patch(updatePlace)
+	.delete(deletePlace);
 
-placesRouter.route('/user/:userId').get(getPlaceByUserId);
+placesRouter.route('/user/:userId').get(getPlacesByUserId);
 
 module.exports = placesRouter;
